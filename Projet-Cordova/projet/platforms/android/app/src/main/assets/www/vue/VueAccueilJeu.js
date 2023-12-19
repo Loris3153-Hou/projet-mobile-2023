5bb71adbf84c5807d96d7ef0a807d4832a632a4c
MathildeBurgevin <129862112+MathildeBurgevin@users.noreply.github.com>
2023-12-19 14:02:13 -0500

    merge

--- Projet-Cordova/projet/platforms/android/app/src/main/assets/www/vue/VueAccueilJeu.js
+++ Projet-Cordova/projet/platforms/android/app/src/main/assets/www/vue/VueAccueilJeu.js
@@ -1,6 +1,7 @@
 class VueAccueilJeu {
     constructor() {
         this.html = document.getElementById("html-vue-accueil-jeu").innerHTML;
+        this.actionAllerPagePersonnalisationJeu = null;
     }
 
     afficher(){
