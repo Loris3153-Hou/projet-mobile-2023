e933e0f48427d2e033c58865e601b1221b4a7026
Nolan10 <nolan.tessier.etu@univ-lemans.fr>
2023-12-19 16:06:36 -0500

    création du système de token

--- serveur/controlleurs/JoueurControlleur.php
+++ serveur/controlleurs/JoueurControlleur.php
@@ -4,13 +4,18 @@ include_once('../DAO/JoueurDAO.php');
 
 $utilisateurDAO = new DAO\JoueurDAO();
 
+if (isset($_GET["token"])){
+    if($_GET["token"] == "valeurAuthentification"){
+        if (isset($_GET["joueur"])) {
+            $html = '';
 
-if (isset($_GET["joueur"])) {
-    $html = '';
+            $utilisateur = $utilisateurDAO->getToutesJoueur();
+            $util = json_encode($utilisateur);
+            //$html .= $utilisateur[0]->getIdJoueur() . $utilisateur[0]->getPseudoJoueur() . $utilisateur[0]->getMeilleurScoreJoueur();
 
-    $utilisateur = $utilisateurDAO->getToutesJoueur();
-    $util = json_encode($utilisateur);
-    //$html .= $utilisateur[0]->getIdJoueur() . $utilisateur[0]->getPseudoJoueur() . $utilisateur[0]->getMeilleurScoreJoueur();
-
-    echo $util;
+            echo $util;
+        }
+    }
 }
+
+
